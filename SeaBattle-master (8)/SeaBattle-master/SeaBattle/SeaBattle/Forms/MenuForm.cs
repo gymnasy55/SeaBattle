6fad584e7bf0bcaae79c66e45218db29e507ef98
gymnasy55 <danyarak7@gmail.com>
2018-03-05 21:57:45 +0200

    Add files via upload

---
+++ SeaBattle-master (8)/SeaBattle-master/SeaBattle/SeaBattle/Forms/MenuForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SeaBattle
+{
+    public partial class MenuForm : Form
+    {
+        public MenuForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Form form = new User1Form();
+            form.Show();
+        }
+
+        private void btnShips_Click(object sender, EventArgs e)
+        {
+            Form form = new ShipsForm();
+            form.ShowDialog();
+        }
+    }
+}
