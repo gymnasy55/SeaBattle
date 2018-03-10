b4d6efce56e243e7d24dfdae4b335c2ad82a04ef
gymnasy55 <danyarak7@gmail.com>
2018-03-10 14:28:43 +0200

    Add files via upload

---
+++ SeaBattle-master (10)/SeaBattle-master/SeaBattle/SeaBattle/Forms/BattleForm.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace SeaBattle
+{
+    partial class BattleForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.tmTime = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblUser.ForeColor = System.Drawing.Color.DeepPink;
+            this.lblUser.Location = new System.Drawing.Point(320, 9);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblUser.Size = new System.Drawing.Size(85, 15);
+            this.lblUser.TabIndex = 0;
+            this.lblUser.Text = "Ход: Первый";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnClose.Location = new System.Drawing.Point(320, 27);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(77, 23);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "Закрыть";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblTime.ForeColor = System.Drawing.Color.Red;
+            this.lblTime.Location = new System.Drawing.Point(344, 53);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(18, 18);
+            this.lblTime.TabIndex = 2;
+            this.lblTime.Text = "0";
+            // 
+            // tmTime
+            // 
+            this.tmTime.Enabled = true;
+            this.tmTime.Interval = 1000;
+            this.tmTime.Tick += new System.EventHandler(this.tmTime_Tick);
+            // 
+            // BattleForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(736, 356);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lblUser);
+            this.Name = "BattleForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Морской Бой";
+            this.Load += new System.EventHandler(this.BattleForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Timer tmTime;
+    }
+}
---
+++ SeaBattle-master (10)/SeaBattle-master/SeaBattle/SeaBattle/Forms/User1Form.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace SeaBattle
+{
+    partial class User1Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNext = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.катерToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.горизонтальныйToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.вертикальныйToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.крейсерToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.горизонтальныйToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.вертикальныйToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.авианосецToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.горизонтальныйToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.вертикальныйToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblShip = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnNext
+            // 
+            this.btnNext.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnNext.Location = new System.Drawing.Point(12, 327);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(630, 23);
+            this.btnNext.TabIndex = 0;
+            this.btnNext.Text = "Next >>>";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.катерToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.крейсерToolStripMenuItem,
+            this.авианосецToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(654, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // катерToolStripMenuItem
+            // 
+            this.катерToolStripMenuItem.Name = "катерToolStripMenuItem";
+            this.катерToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.катерToolStripMenuItem.Text = "Катер";
+            this.катерToolStripMenuItem.Click += new System.EventHandler(this.катерToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.горизонтальныйToolStripMenuItem,
+            this.вертикальныйToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(68, 20);
+            this.toolStripMenuItem1.Text = "Эсминец";
+            // 
+            // горизонтальныйToolStripMenuItem
+            // 
+            this.горизонтальныйToolStripMenuItem.Name = "горизонтальныйToolStripMenuItem";
+            this.горизонтальныйToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
+            this.горизонтальныйToolStripMenuItem.Text = "Горизонтальный";
+            this.горизонтальныйToolStripMenuItem.Click += new System.EventHandler(this.горизонтальныйToolStripMenuItem_Click);
+            // 
+            // вертикальныйToolStripMenuItem
+            // 
+            this.вертикальныйToolStripMenuItem.Name = "вертикальныйToolStripMenuItem";
+            this.вертикальныйToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
+            this.вертикальныйToolStripMenuItem.Text = "Вертикальный";
+            this.вертикальныйToolStripMenuItem.Click += new System.EventHandler(this.вертикальныйToolStripMenuItem_Click);
+            // 
+            // крейсерToolStripMenuItem
+            // 
+            this.крейсерToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.горизонтальныйToolStripMenuItem1,
+            this.вертикальныйToolStripMenuItem1});
+            this.крейсерToolStripMenuItem.Name = "крейсерToolStripMenuItem";
+            this.крейсерToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.крейсерToolStripMenuItem.Text = "Крейсер";
+            // 
+            // горизонтальныйToolStripMenuItem1
+            // 
+            this.горизонтальныйToolStripMenuItem1.Name = "горизонтальныйToolStripMenuItem1";
+            this.горизонтальныйToolStripMenuItem1.Size = new System.Drawing.Size(167, 22);
+            this.горизонтальныйToolStripMenuItem1.Text = "Горизонтальный";
+            this.горизонтальныйToolStripMenuItem1.Click += new System.EventHandler(this.горизонтальныйToolStripMenuItem1_Click);
+            // 
+            // вертикальныйToolStripMenuItem1
+            // 
+            this.вертикальныйToolStripMenuItem1.Name = "вертикальныйToolStripMenuItem1";
+            this.вертикальныйToolStripMenuItem1.Size = new System.Drawing.Size(167, 22);
+            this.вертикальныйToolStripMenuItem1.Text = "Вертикальный";
+            this.вертикальныйToolStripMenuItem1.Click += new System.EventHandler(this.вертикальныйToolStripMenuItem1_Click);
+            // 
+            // авианосецToolStripMenuItem
+            // 
+            this.авианосецToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.горизонтальныйToolStripMenuItem2,
+            this.вертикальныйToolStripMenuItem2});
+            this.авианосецToolStripMenuItem.Name = "авианосецToolStripMenuItem";
+            this.авианосецToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.авианосецToolStripMenuItem.Text = "Линкор";
+            // 
+            // горизонтальныйToolStripMenuItem2
+            // 
+            this.горизонтальныйToolStripMenuItem2.Name = "горизонтальныйToolStripMenuItem2";
+            this.горизонтальныйToolStripMenuItem2.Size = new System.Drawing.Size(167, 22);
+            this.горизонтальныйToolStripMenuItem2.Text = "Горизонтальный";
+            this.горизонтальныйToolStripMenuItem2.Click += new System.EventHandler(this.горизонтальныйToolStripMenuItem2_Click);
+            // 
+            // вертикальныйToolStripMenuItem2
+            // 
+            this.вертикальныйToolStripMenuItem2.Name = "вертикальныйToolStripMenuItem2";
+            this.вертикальныйToolStripMenuItem2.Size = new System.Drawing.Size(167, 22);
+            this.вертикальныйToolStripMenuItem2.Text = "Вертикальный";
+            this.вертикальныйToolStripMenuItem2.Click += new System.EventHandler(this.вертикальныйToolStripMenuItem2_Click);
+            // 
+            // lblShip
+            // 
+            this.lblShip.AutoSize = true;
+            this.lblShip.Location = new System.Drawing.Point(61, 24);
+            this.lblShip.Name = "lblShip";
+            this.lblShip.Size = new System.Drawing.Size(41, 13);
+            this.lblShip.TabIndex = 6;
+            this.lblShip.Text = "ничего";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-3, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Выбрано: ";
+            // 
+            // User1Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 352);
+            this.Controls.Add(this.lblShip);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "User1Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Расставте корабли";
+            this.Load += new System.EventHandler(this.User1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem катерToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem горизонтальныйToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem вертикальныйToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem крейсерToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem горизонтальныйToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem вертикальныйToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem авианосецToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem горизонтальныйToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem вертикальныйToolStripMenuItem2;
+        private System.Windows.Forms.Label lblShip;
+        private System.Windows.Forms.Label label1;
+    }
+}
